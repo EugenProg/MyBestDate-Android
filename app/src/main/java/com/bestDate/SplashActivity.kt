956e1edd7b5f94e1ef1f6877e787fd3414f65f69
alicelebedeva <alisalebedeva28@mail.ru>
2022-11-22 17:45:03 +0600

    feature: create bottom nav view with custom buttons; init badge; remove MainScreenActivity;

--- app/src/main/java/com/bestDate/SplashActivity.kt
+++ app/src/main/java/com/bestDate/SplashActivity.kt
@@ -3,14 +3,13 @@ package com.bestDate
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.bestDate.presentation.main.MainScreenActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainScreenActivity::class.java))
+        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
 
---
+++ app/src/main/java/com/bestDate/view/bottomNav/CustomBottomNavButtonView.kt
@@ -0,0 +1,66 @@
+package com.bestDate.view.bottomNav
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.core.view.isVisible
+import com.bestDate.data.extension.setOnSaveClickListener
+import com.bestDate.databinding.ViewBottomNavButtonBinding
+
+class CustomBottomNavButtonView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+    private var binding: ViewBottomNavButtonBinding =
+        ViewBottomNavButtonBinding.inflate(LayoutInflater.from(context), this)
+
+    var onClick: (() -> Unit)? = null
+    var onNavigationChange: (() -> Unit)? = null
+
+    var isActive: Boolean = false
+        set(value) {
+            binding.activeImageView.isVisible = value
+            binding.labelTextView.isVisible = !value
+            if (value) {
+                binding.iconImageView.setImageResource(iconActive)
+            } else {
+                binding.iconImageView.setImageResource(icon)
+            }
+            field = value
+        }
+
+    var icon: Int = 0
+        set(value) {
+            if (!isActive) {
+                binding.iconImageView.setImageResource(value)
+            }
+            field = value
+        }
+
+    var iconActive: Int = 0
+        set(value) {
+            if (isActive) {
+                binding.iconImageView.setImageResource(value)
+            }
+            field = value
+        }
+
+    var label: String = ""
+        set(value) {
+            binding.labelTextView.text = value
+            field = value
+        }
+
+    var hasBadge: Boolean = true
+        set(value) {
+            binding.viewBadge.isVisible = value
+            field = value
+        }
+
+    init {
+        binding.root.setOnSaveClickListener {
+            onClick?.invoke()
+            onNavigationChange?.invoke()
+        }
+    }
+}
