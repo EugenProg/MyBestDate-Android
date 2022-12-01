18833d9a1c5284c0c12e166e4b276b56683882ad
eugen <Evko90@Yandex.ru>
2022-12-01 22:40:05 +0600

    feature: create a default alert dialog

--- app/src/main/java/com/bestDate/data/extension/DialogExtension.kt
+++ app/src/main/java/com/bestDate/data/extension/DialogExtension.kt
@@ -1,7 +1,12 @@
 package com.bestDate.data.extension
 
+import android.app.Dialog
+import android.view.Gravity
+import android.view.View
+import android.widget.LinearLayout
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
+import com.bestDate.R
 
 fun DialogFragment?.show(fragmentManager: FragmentManager) {
     if (this?.isAdded == false && !this.isVisible) {
@@ -13,4 +18,18 @@ fun DialogFragment?.show(fragmentManager: FragmentManager) {
     } else {
         return
     }
+}
+
+fun getDialog(
+    view: View,
+    position: Int = Gravity.CENTER,
+    width: Int = LinearLayout.LayoutParams.MATCH_PARENT,
+    height: Int = LinearLayout.LayoutParams.WRAP_CONTENT
+): Dialog {
+    val dialog = Dialog(view.context, R.style.dialog_theme)
+    dialog.setContentView(view)
+    dialog.window?.setLayout(width, height)
+    dialog.window?.setGravity(position)
+    dialog.show()
+    return dialog
 }
