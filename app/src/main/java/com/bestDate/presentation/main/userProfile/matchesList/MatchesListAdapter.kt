4374cb5405d948870f5269dbad49e690b17ba5fc
EugenProg <Evkocub90@gmail.com>
2023-04-05 23:53:54 +0600

    feature: matches to paging

--- app/src/main/java/com/bestDate/presentation/main/userProfile/matchesList/MatchesListAdapter.kt
+++ app/src/main/java/com/bestDate/presentation/main/userProfile/matchesList/MatchesListAdapter.kt
@@ -2,16 +2,16 @@ package com.bestDate.presentation.main.userProfile.matchesList
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import com.bestDate.presentation.base.BaseClickViewHolder
 import com.bestDate.data.extension.setOnSaveClickListener
 import com.bestDate.data.model.Match
 import com.bestDate.databinding.ItemMatchesListBinding
+import com.bestDate.presentation.base.BaseClickViewHolder
 import com.bumptech.glide.Glide
 
 class MatchesListAdapter(private val myPhotoUrl: String) :
-    ListAdapter<Match, MatchesListAdapter.MatchesListViewHolder>(MatchesDiffUtils()) {
+    PagingDataAdapter<Match, MatchesListAdapter.MatchesListViewHolder>(MatchesDiffUtils()) {
 
     var itemClick: ((Match, MatchesSelectType) -> Unit)? = null
 
@@ -25,35 +25,41 @@ class MatchesListAdapter(private val myPhotoUrl: String) :
         }
     }
 
-    class MatchesListViewHolder(override val binding: ItemMatchesListBinding, private val myPhotoUrl: String) :
-        BaseClickViewHolder<Match, ((Match, MatchesSelectType) -> Unit)?, ItemMatchesListBinding>(binding) {
-        override fun bindView(item: Match, itemClick: ((Match, MatchesSelectType) -> Unit)?) {
+    class MatchesListViewHolder(
+        override val binding: ItemMatchesListBinding,
+        private val myPhotoUrl: String
+    ) :
+        BaseClickViewHolder<Match?, ((Match, MatchesSelectType) -> Unit)?, ItemMatchesListBinding>(
+            binding
+        ) {
+        override fun bindView(item: Match?, itemClick: ((Match, MatchesSelectType) -> Unit)?) {
             with(binding) {
                 Glide.with(itemView.context)
-                    .load(item.user?.main_photo?.thumb_url)
+                    .load(item?.user?.main_photo?.thumb_url)
                     .circleCrop()
                     .into(avatar)
                 Glide.with(itemView.context)
                     .load(myPhotoUrl)
                     .into(myPhoto)
                 Glide.with(itemView.context)
-                    .load(item.user?.main_photo?.thumb_url)
+                    .load(item?.user?.main_photo?.thumb_url)
                     .into(userPhoto)
-                name.text = item.user?.name
-                location.text = item.user?.getLocation()
-                time.text = item.getTime()
-                verifyView.isVerified = item.user?.full_questionnaire
+                name.text = item?.user?.name
+                location.text = item?.user?.getLocation()
+                time.text = item?.getTime()
+                verifyView.isVerified = item?.user?.full_questionnaire
 
-                root.setOnSaveClickListener {
-                    itemClick?.invoke(item, MatchesSelectType.USER)
-                }
+                item?.let { match ->
+                    root.setOnSaveClickListener {
+                        itemClick?.invoke(match, MatchesSelectType.USER)
+                    }
 
-                matchesBox.setOnSaveClickListener {
-                    itemClick?.invoke(item, MatchesSelectType.MATCH)
+                    matchesBox.setOnSaveClickListener {
+                        itemClick?.invoke(match, MatchesSelectType.MATCH)
+                    }
                 }
             }
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchesListViewHolder {
